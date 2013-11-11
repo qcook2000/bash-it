b912bd83fc43aa0ab89f9577920db27ced4f829f
Quenton Cook <quenton@spotify.com>
2013-11-12 00:37:35 +0100

    Hello

--- update-me.sh
+++ update-me.sh
@@ -1,9 +1,9 @@
 if [ "$(uname)" = "Darwin" ]; then
   # Do something under Mac OS X platform
-  sh scripts/update-me-osx.sh;
+  sh $BASH_IT/scripts/update-me-osx.sh;
 elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
   # Do something under Linux platform
-  sh scripts/update-me-linux.sh;
+  sh $BASH_IT/scripts/update-me-linux.sh;
 elif [ "$(expr substr $(uname -s) 1 10)" = "MINGW32_NT" ]; then
   # Do something under Windows NT platform
   # sh scripts/update-me-win.sh;
